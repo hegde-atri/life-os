c5923b822fbbfdecc77d58be1c39f8aabcdc3760
Annie Tse <annietse03@gmail.com>
2023-11-11 21:46:44 +0000

    connected settings pages to tabs

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React from 'react'
+import { redirect, usePathname } from "next/navigation";
+import { Tab, Tabs } from '@nextui-org/react';
+import Categories from './categories/page';
+import Preferences from './preferences/page';
+import Account from './account/page';
+
+export const Settings = () => {
+  return (
+  <div>
+<div className="flex flex-col gap-4">
+        <Tabs radius="md" aria-label="Tabs radius" className="mx-auto my-10 text">
+          <Tab key="categories" title="Categories" className="text-md mx-auto"><Categories></Categories></Tab>
+          <Tab key="preferences" title="Preferences" className="text-md mx-auto"><Preferences></Preferences></Tab>
+          <Tab key="account" title="Account" className="text-md mx-auto"><Account></Account></Tab>
+        </Tabs>
+    </div>
+    
+  </div>
+  )
+}
+
+export default Settings;
