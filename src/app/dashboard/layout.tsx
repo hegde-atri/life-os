91ee1dc12643ee125e687aa598e18a568bcce8a5
Nick Michau <70205169+NOM1989@users.noreply.github.com>
2023-11-11 16:08:26 +0000

    update: Protected dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,18 @@
 import { Header } from "../_components/Header";
+import { redirect } from 'next/navigation';
+import { getServerAuthSession } from "~/server/auth";
 
-export default function DashboardLayout({
+export default async function  DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+
+  if (!session?.user.id) {
+    redirect("/")
+  }
+
   return (
     <>
       <Header />
@@ -12,3 +20,4 @@ export default function DashboardLayout({
     </>
   );
 }
+
