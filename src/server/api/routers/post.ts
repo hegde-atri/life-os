ffd3080363f38d48ed0b70323d40546fdd9fc257
Atri Hegde <iamatrihegde@outlook.com>
2023-11-11 12:47:20 +0000

    (fix): type error

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,11 +1,8 @@
-import { z } from "zod";
-
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-import { posts } from "~/server/db/schema";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure.query(async () => {
@@ -16,23 +13,17 @@ export const postRouter = createTRPCRouter({
     };
   }),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  // create: protectedProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     // simulate a slow db call
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      await ctx.db.insert(posts).values({
-        name: input.name,
-        createdById: ctx.session.user.id,
-      });
-    }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.query.posts.findFirst({
-      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
-    });
-  }),
+  //     await ctx.db.insert(posts).values({
+  //       name: input.name,
+  //       createdById: ctx.session.user.id,
+  //     });
+  //   }),
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
