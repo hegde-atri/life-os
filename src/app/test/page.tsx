d7ba693534c0fd6d1d9897310de4150bb540ca75
Nick Michau <70205169+NOM1989@users.noreply.github.com>
2023-11-12 12:32:37 +0000

    add: Add Task funciton

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { api } from "~/trpc/react";
 import { Task } from "../_components/Task";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   const { data: json_tasks, isLoading } = api.post.hello.useQuery();
@@ -11,14 +12,12 @@ export default function Home() {
     return <></>;
   }
 
+  let { mutate } = api.post.createTask.useMutation();
+
   return (
     <main className="text-medium">
       <div className="grid grid-cols-2 gap-2 sm:grid-cols-4">
-        {Object.entries(json_tasks.data).map(([category, rest]) =>
-          Object.entries(rest).map(([task, value]) => (
-            <Task category={category} task={task} value={value} />
-          )),
-        )}
+        <Button onClick={() => mutate({ category: "jumping", name: "Jump for 10 hours straight", points: 10 })}></Button>
       </div>
     </main>
   );
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,6 +5,7 @@ import {
 } from "~/server/api/trpc";
 
 import OpenAI from "openai";
+import { z } from "zod";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -12,29 +13,62 @@ const openai = new OpenAI({
 
 export const postRouter = createTRPCRouter({
   hello: protectedProcedure.query(async () => {
-    // const count = 2;
+    const count = 2;
 
-    // const chatCompletion = await openai.chat.completions.create({
-    //   messages: [
-    //     {
-    //       role: "system",
-    //       content: `Generate ${count} *self-enclosing* personal development tasks that someone could do for each category provided, assign each a corresponding difficulty value (0-40). Format json: {category: {task: value, task: value...},}. Don't add markdown. Eg. {"Swimming": {"Swim 500m non-stop": 15,...},}`,
-    //     },
-    //     {
-    //       role: "user",
-    //       content: "Uni Work, Programming, Swimming, over sleeping",
-    //     },
-    //   ],
-    //   model: "gpt-3.5-turbo-1106",
-    // });
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content:
+            `Generate ${count} *self-enclosing* personal development tasks that someone could do for each category provided, assign each a corresponding difficulty value (0-40). Format json: {category: {task: value, task: value...},}. Don't add markdown. Eg. {"Swimming": {"Swim 500m non-stop": 15,...},}`,
+        },
+        {
+          role: "user",
+          content: "Uni Work, Programming, Swimming, over sleeping",
+        },
+      ],
+      model: "gpt-3.5-turbo-1106",
+    });
 
-    // console.log(chatCompletion.choices[0]?.message.content);
+    console.log(chatCompletion.choices[0]?.message.content);
 
     return {
-      data: "message",
-      // data: JSON.parse(chatCompletion.choices[0]?.message.content!),
+      // data: "message",
+      data: JSON.parse(chatCompletion.choices[0]?.message.content!),
     };
   }),
+
+  createTask: protectedProcedure
+    .input(
+      z.object({
+        category: z.string().min(1),
+        name: z.string(),
+        points: z.number(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      let categoryFromName = await ctx.db.category.findFirst({
+        where: { name: input.category },
+      });
+
+      let categoryId;
+      if (categoryFromName) {
+        categoryId = categoryFromName.id;
+      } else {
+        categoryFromName = await ctx.db.category.findFirst();
+        categoryId = categoryFromName?.id!
+      }
+
+      await ctx.db.task.create({
+        data: {
+          userId: ctx.session.user.id,
+          categoryId: categoryId,
+          name: input.name,
+          points: input.points,
+        },
+      });
+      return { ok: true };
+    }),
 });
 
 // const example_data = `{
