6a5d74c03084bbf02bd185552ec0267d8bac8653
Nick Michau <70205169+NOM1989@users.noreply.github.com>
2023-12-26 18:20:32 +0000

    add: Inital attempt to edit task backend

--- src/app/_components/TaskModal.tsx
+++ src/app/_components/TaskModal.tsx
@@ -17,37 +17,48 @@ export const TaskModal = (props: {
   category: string;
   task: string;
   points: number;
+  // setTask: (newTask: string) => void; // Add a new prop function to update task
+  // setPoints: (newPoints: number) => void; // Add a new prop function to update points
 }) => {
+  var task = props.task
+  var points = props.points
+
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [inputTask, setInputTask] = React.useState(task); // State to keep track of the input value
+  const [inputPoints, setInputPoints] = React.useState(points.toString()); // State to keep track of the input value
 
   return (
     <>
-      <Button isIconOnly onPress={onOpen} className="absolute -top-4 -right-4 z-50 opacity-85 rounded-full">
+      <Button
+        isIconOnly
+        onPress={onOpen}
+        className="opacity-85 absolute -right-4 -top-4 z-50 rounded-full"
+      >
         <TbPencil />
       </Button>
-      <Task category={props.category} task={props.task} points={props.points}/>
-      <Modal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        placement="top-center"
-      >
+      <Task category={props.category} task={task} points={points} />
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex gap-2 items-center">
+              <ModalHeader className="flex items-center gap-2">
                 Edit Task <TbPencil />
               </ModalHeader>
               <ModalBody>
-                <div className="flex justify-between items-center gap-2">
+                <div className="flex items-center justify-between gap-2">
                   <Chip color="primary">{props.category}</Chip>
                   <Input
                     className="w-unit-24"
                     type="number"
-                    defaultValue={props.points.toString()}
+                    // defaultValue={props.points.toString()}
+                    value={inputPoints} // Set the value of the input to the state
+                    onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setInputPoints(e.target.value)
+                    } // Update the state as the input value changes
                     labelPlacement="outside"
                     startContent={
                       <div className="pointer-events-none flex items-center">
-                        <span className="text-default-400 text-small">
+                        <span className="text-small text-default-400">
                           <TbCoins />
                         </span>
                       </div>
@@ -58,11 +69,24 @@ export const TaskModal = (props: {
                   autoFocus
                   variant="bordered"
                   label="Task"
-                  defaultValue={props.task}
+                  // defaultValue={props.task}
+                  value={inputTask} // Set the value of the input to the state
+                  onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setInputTask(e.target.value)
+                  } // Update the state as the input value changes
                 />
               </ModalBody>
               <ModalFooter>
-                <Button color="primary" onPress={onClose}>
+                <Button
+                  color="primary"
+                  onPress={() => {
+                    task = inputTask
+                    points = parseInt(inputPoints)
+                    // props.setTask(inputTask); // Call the function to update the task
+                    // props.setPoints(parseInt(inputPoints)); // Call the function to update the points
+                    onClose(); // Close the modal
+                  }}
+                >
                   Save
                 </Button>
               </ModalFooter>
